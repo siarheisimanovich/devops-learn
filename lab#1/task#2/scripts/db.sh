ae539aae034cc96850a239049a2cae354a0adb92
dearsimanovich <siarhei.simanovich@headspace.com>
2022-08-15 20:39:40 +0200

    Fix list & list --inverse command

--- lab#1/task#2/scripts/db.sh
+++ lab#1/task#2/scripts/db.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 dbfile=../data/users.db
-# +
+inverse=$2
+
 function createFile () {
 	if [ ! -f $dbfile ]; then
 		touch $dbfile;
 	fi
 }
-# +
+
 function listItems () {
-	declare -i i=1
-	while read user
-	do 
-		echo "$i: $user"
-		(( i++ ))
-	done < ../data/users.db
+	if [ -z $inverse ]; then
+		nl $dbfile
+	else
+		nl $dbfile | sort -nr
+	fi
+	
 }
-# +
+
 function add () {
 	createFile;
 	read -p "Enter user name: " username
@@ -39,17 +40,17 @@ function add () {
 	done
 	echo "$username $userrole" >> ../data/users.db
 }
-# +
+
 function backup () {
 	date=`date +"%d-%m-%y"`
-	mv ../data/users.db ../data/${date}-users.db.backup
+	cp ../data/users.db ../data/${date}-users.db.backup
 }
-# -
+
 function restore () {
 	latest=$(find ../data -name '*.backup' -type f) # this not save in variable and not latest
 	mv $latest ../data/users.db
 }
-# -
+
 function find () {
 	read -p "Enter user name: " username
 	while [ -z $username ]; # Q: How I can pass string with space without double quotes?
@@ -58,42 +59,32 @@ function find () {
 	done
 	
 	result=listItems | grep $username;
-	echo $result
+
 	if [ !$result ]; then
-	echo 'User not found'
+		echo 'User not found'
 	else
-	echo $result
+		echo $result
 	fi
 
 }
-# +/-
+
 function list () {
-	#while getopt -l inverse: option; 
-	#do
-	#	case $option in
-	#	 inverse) echo $option exit 1;;
-	#	 *) echo no option exit 1;;
-	#	 esac
-	#done
 	listItems;
 }
-# +
 function help () {
-{
 echo -n " 
-Commands:
-	add       	Add user to the database file
-	backup    	Create backup of database file
-	restore		Resore latest backup
-	find       	Find user in database by name
-	list		List all the users in database file
-	
-Options:
-	--inverse	Show reversed list command output 
+	Commands:
+		add				Add user to the database file
+		backup			Create backup of database file
+		restore			Resore latest backup
+		find			Find user in database by name
+		list			List all the users in database file
+		
+	Options:
+		--inverse		Show reversed list command output 
 "
-exit 1
-}}
-# +
+}
+
 case $1 in 
 	"add") add;;
 	"help") help;;
